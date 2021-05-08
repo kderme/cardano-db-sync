dda0d1248589b87135068916beb6bec029e20575
Erik de Castro Lopo <erikd@mega-nerd.com>
2021-05-08 14:29:29 +1000

    wip

--- cardano-sync/src/Cardano/Sync/LedgerState.hs
+++ cardano-sync/src/Cardano/Sync/LedgerState.hs
@@ -587,9 +587,12 @@ ledgerEpochNo :: LedgerEnv -> CardanoLedgerState -> EpochNo
 ledgerEpochNo env cls =
     case ledgerTipSlot (ledgerState (clsState cls)) of
       Origin -> 0 -- An empty chain is in epoch 0
-      NotOrigin slot -> runIdentity $ epochInfoEpoch epochInfo slot
+      NotOrigin slot ->
+        case runExcept $ epochInfoEpoch epochInfo slot of
+          Left err -> panic $ "ledgerEpochNo: " <> textShow err
+          Right en -> en
   where
-    epochInfo :: EpochInfo Identity
+    epochInfo :: EpochInfo (Except Consensus.PastHorizonException)
     epochInfo = epochInfoLedger (configLedger $ topLevelConfig env) (hardForkLedgerStatePerEra . ledgerState $ clsState cls)
 
 -- Like 'Consensus.tickThenReapply' but also checks that the previous hash from the block matches
